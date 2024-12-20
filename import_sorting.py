d061d90bad837c1837421ab88635e366745a5f31
Jeremy Monat <jemonat@calalum.org>
2024-12-19 22:17:04 -0500

    Indigo examples, lastly draw molecule to Jupyter notebook

--- import_sorting.py
+++ import_sorting.py
@@ -1,10 +1,3 @@
-import math
-
-import matplotlib.pyplot as plt
-import numpy as np
-import polars as pl
-import seaborn
 from py2opsin import py2opsin
-from rdkit import Chem
-from rdkit.Chem import Descriptors
-from rdkit.Chem.Draw import IPythonConsole
+
+py2opsin("C")
